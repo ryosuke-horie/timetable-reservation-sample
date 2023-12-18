73860701456b414d956ffafd1b74a6ae8fb1feaa
ryosuke-horie <ryosuke03140@gmial.com>
2023-12-18 19:32:30 +0900

    eslint, prettierの設定

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "13",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser"
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
