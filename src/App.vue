641a09c7a9aeb5174cbb6929719a5d9ace670a10
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-12-20 12:59:34 +0900

    タイムライン形式でVue-calを利用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
   import "ress";
+  import VueCal from "vue-cal";
+  import "vue-cal/dist/vuecal.css";
 </script>
 
 <template>
-  <div>test</div>
+  <h1>TimeSchedule</h1>
+  <vue-cal
+    :disable-views="['years', 'year', 'month']"
+    active-view="week"
+    :time-from="10 * 60"
+    :time-to="24 * 60"
+    :time-step="60"
+  />
 </template>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    margin-top: 1em;
+    margin-bottom: 0.5em;
+  }
+</style>
