91c8a819a8981db5fced1badcf2504c61d5106ba
ryosuke-horie <ryosuke03140@gmial.com>
2023-12-20 18:24:07 +0900

    ビルドを通すため、TypeScript設定を変更。

---
+++ types/vue-cal.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "vue-cal" {
+  const VueCal: any;
+  export default VueCal;
+}
+/* eslint-enable @typescript-eslint/no-explicit-any */
